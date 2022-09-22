b19a6582b1372aa59f00d934715f1cf7f56c487a
Omar Eldanasoury <30899458+omar-danasoury@users.noreply.github.com>
2022-09-22 21:13:59 +0300

    Added javadoc comment for recursive_gcd()
    Added javadoc comment for recursive_gcd() in Euclid.java

--- Ch01 Introduction to Analysis and Design of Algorithms/GCD - Euclid's Algorithm/Euclid.java
+++ Ch01 Introduction to Analysis and Design of Algorithms/GCD - Euclid's Algorithm/Euclid.java
@@ -2,4 +2,17 @@ public class Euclid {
     public static void main(String[] args) {
         
     }
+
+    /**
+     * Applies the algorithm of Euclid's to solve the gcd
+     * problem recursively.
+     * The Pseodocode can be found at ../README.md.
+     *
+     * @param m first non-negative integer, m != 0 if n = 0.
+     * @param n second non-negative integer, n != 0 if m = 0.
+     * @return greatest common divisor between m and n
+     */
+    public int recersive_gcd(int m, int n){
+
+    }
 }
